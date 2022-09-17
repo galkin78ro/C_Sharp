d00ce2f6c3267a88fb5fb166e67a77808093fade
Galkin Stanislav <galkin@yandex.ru>
2022-09-18 00:52:17 +0300

    Задача 2. Программа для сравнения двух чисел с правками

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Программа для сравнения двух чисел");
-System.Console.WriteLine("Введите число1");
+Console.WriteLine("Введите число1");
 int a = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите число2");
+Console.WriteLine("Введите число2");
 int b = int.Parse(Console.ReadLine());
 if (a > b)
 {
-    System.Console.WriteLine("Число1 больше числа2");
+    Console.WriteLine("Число1 больше числа2");
 }else if (a < b)
 {
     Console.WriteLine("Число2 больше числа1");
