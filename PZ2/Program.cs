d5c9c64523d1000eebe5da672ea52f5bd3f19fea
Galkin Stanislav <galkin@yandex.ru>
2022-09-23 03:00:48 +0300

    Программа для вывода третьей цифры в трехзначном числе написана

--- PZ2/Program.cs
+++ PZ2/Program.cs
@@ -11,11 +11,33 @@
     { 
         Console.WriteLine("Введено недопустимое число");}
 }
+void Problem13 ()
+{
+    Console.WriteLine("Программа для вывода третьей цифры в трехзначном числе");
+    Console.WriteLine("Введите трехзначное число");
+    int y = int.Parse(Console.ReadLine());
+    if ( y is > 9 and < 100 )
+    {
+        Console.WriteLine("В введенном числе отсутствует третья цифра");
+        return;}
+    if ( y is > 9 and < 1000 )
+    {   
+        int d = y % 10;
+        Console.WriteLine("В введенном числе, третья цифра - " + d );}
+    else 
+    { 
+        Console.WriteLine("Введено недопустимое число");}
+}
 Console.WriteLine("Введите номер задания");
 int x = int.Parse(Console.ReadLine());
 if (x == 10)
     {
-    Problem10 ();}
+    Problem10 ();
+    return;}
+if (x == 13)
+    {
+    Problem13 ();
+    return;}
 else 
     {
     Console.WriteLine("Задания с таким номером не обнаружено");}
